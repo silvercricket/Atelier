33df3a2f63e3e6c1c68d547bf8eb3373ef6d486c
Andrew Myronchuk <myronchukmail@gmail.com>
2025-01-23 18:36:31 -0500

    set up QA component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductOverview from './ProductOverview/ProductOverview.jsx';
+import QA from './QA/QA.jsx';
 
 var App = (props) => {
 
   return (
     <div>
       <ProductOverview />
+      <QA />
       <p>hello world</p>
     </div>
   );
---
+++ client/src/components/QA/QA.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QA = () => {
+  return (
+    <section>
+      <h4>Questions and Answers</h4>
+    </section>
+  )
+};
+
+export default QA;
