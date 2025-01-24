dccb0ed0b73f79b5986fd2ebc1c721b89929f3c6
tranbonnie <tran.bonniee@gmail.com>
2025-01-24 15:16:33 -0800

    implemented individual comparison cards with test data

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import RelatedItemCard from './RelatedItemCard.jsx'
+import { showDetailView, showPreviousCard, showNextCard, addToOutfit } from '../../store/relatedItemsSlice.js';
 import Outfit from './Outfit.jsx'
 import axios from 'axios';
 
 const RelatedItems = () => {
+
+  const dispatch = useDispatch();
+
+  const itemCard = useSelector((state) => {
+    return state.relatedItems.relatedItems[state.relatedItems.currentCardIndex]
+  })
 
   const relatedItems = useSelector((state) => {
     return state.relatedItems.relatedItems
@@ -12,8 +19,13 @@ const RelatedItems = () => {
 
   return (
     <div>
-      {/* <RelatedItemCard /> */}
-      {relatedItems.map((item) => <RelatedItemCard name = {item.name} />)}
+      <h2>Related Items</h2>
+      {relatedItems.map((item) => <RelatedItemCard category = {item.category} name = {item.name} price = {item.default_price}/>)}
+      <div>
+      <button onClick = {() => {dispatch(showPreviousCard())}}>Show Previous Item</button>
+      <button onClick = {() => {dispatch(showNextCard())}}>Show Next Item</button>
+      </div>
+      <h2>My Outfit</h2>
       <Outfit />
     </div>
   )
