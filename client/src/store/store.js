65c6df04797846ab7e7e2c5fc38c05289178f638
Lucas <lucascostamonteiro@yahoo.com>
2025-01-24 11:09:10 -0800

    jest testing

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import productOverviewReducer from './productOverviewSlice.js';
 import ratingsReviewsReducer from './ratingsReviewsSlice.js';
 import relatedItemsReducer from './relatedItemsSlice.js';
 import productsReducer from './productsSlice.js';
@@ -8,7 +7,6 @@ import cartReducer from './cartSlice.js';
 
 export const store = configureStore({
   reducer: {
-    productOverview: productOverviewReducer,
     ratingsReviews: ratingsReviewsReducer,
     relatedItems: relatedItemsReducer,
     products: productsReducer,
