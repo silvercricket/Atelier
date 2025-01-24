dcfa6184372e43e92d42f1a26f1a789356ed7e8b
Byron Work <workbyron22@gmail.com>
2025-01-23 20:15:01 -0500

    Added star rating progress bars that reflect input data

--- client/src/components/RatingsReviews/ratingBreakdown.jsx
+++ client/src/components/RatingsReviews/ratingBreakdown.jsx
@@ -26,15 +26,73 @@ const RatingBreakdown = () => {
   })
 
 
+
+  // disgusting wet code I can't figure out how to pass in a star value and only make one function :(
+    const fiveStar = useSelector((state) => {
+      const reviews = state.ratingsReviews.reviews;
+      let starCount = 0;
+      for (let review of reviews) {
+        if (review.rating === 5) {
+          starCount++;
+        }
+      }
+      return Math.round((starCount / reviews.length) * 100);
+    });
+
+  const fourStar = useSelector((state) => {
+    const reviews = state.ratingsReviews.reviews;
+    let starCount = 0;
+    for (let review of reviews) {
+      if (review.rating === 4) {
+        starCount++;
+      }
+    }
+    return Math.round((starCount / reviews.length) * 100);
+  });
+
+  const threeStar = useSelector((state) => {
+    const reviews = state.ratingsReviews.reviews;
+    let starCount = 0;
+    for (let review of reviews) {
+      if (review.rating === 3) {
+        starCount++;
+      }
+    }
+    return Math.round((starCount / reviews.length) * 100);
+  });
+
+  const twoStar = useSelector((state) => {
+    const reviews = state.ratingsReviews.reviews;
+    let starCount = 0;
+    for (let review of reviews) {
+      if (review.rating === 2) {
+        starCount++;
+      }
+    }
+    return Math.round((starCount / reviews.length) * 100);
+  });
+
+  const oneStar = useSelector((state) => {
+    const reviews = state.ratingsReviews.reviews;
+    let starCount = 0;
+    for (let review of reviews) {
+      if (review.rating === 1) {
+        starCount++;
+      }
+    }
+    return Math.round((starCount / reviews.length) * 100);
+  });
+
+
   return (
     <div>
       <h2>{avgRating}</h2><p>STAR ICONS HERE</p>
       <p>{percentRec}% of reviews reccomend this product</p>
-      <p>5 stars</p><progress value="60" max="100"></progress>
-      <p>4 stars</p><progress value="20" max="100"></progress>
-      <p>3 stars</p><progress value="0" max="100"></progress>
-      <p>2 stars</p><progress value="15" max="100"></progress>
-      <p>1 stars</p><progress value="5" max="100"></progress>
+      <p>5 stars</p><progress value={fiveStar} max="100"></progress>
+      <p>4 stars</p><progress value={fourStar} max="100"></progress>
+      <p>3 stars</p><progress value={threeStar} max="100"></progress>
+      <p>2 stars</p><progress value={twoStar} max="100"></progress>
+      <p>1 stars</p><progress value={oneStar} max="100"></progress>
     </div>
 
   );
