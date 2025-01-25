75ea4e81b5b93a65ad729030cac9776bebb8275d
tranbonnie <tran.bonniee@gmail.com>
2025-01-24 16:58:46 -0800

    added floating related item card and add to outfit functionality

--- client/src/store/relatedItemsSlice.js
+++ client/src/store/relatedItemsSlice.js
@@ -1,6 +1,25 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const currentTestItem = {
+  "id": 40344,
+  "campus": "hr-rfp",
+  "name": "Camo Onesie",
+  "slogan": "Blend in to your crowd",
+  "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
+  "category": "Jackets",
+  "default_price": "140.00",
+  "created_at": "2021-08-13T14:38:44.509Z",
+  "updated_at": "2021-08-13T14:38:44.509Z"
+}
+
+const relatedItemIds = [
+  40345,
+  40346,
+  40351,
+  40350
+];
+
 const testData = [{
   "id": 40344,
   "campus": "hr-rfp",
@@ -57,9 +76,108 @@ const testData = [{
   "updated_at": "2021-08-13T14:38:44.509Z"
 }];
 
+const testDataRelatedItems = [
+  {
+    "id": 40345,
+    "campus": "hr-rfp",
+    "name": "Bright Future Sunglasses",
+    "slogan": "You've got to wear shades",
+    "description": "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
+    "category": "Accessories",
+    "default_price": "69.00",
+    "created_at": "2021-08-13T14:38:44.509Z",
+    "updated_at": "2021-08-13T14:38:44.509Z",
+    "features": [
+        {
+            "feature": "Lenses",
+            "value": "Ultrasheen"
+        },
+        {
+            "feature": "UV Protection",
+            "value": null
+        },
+        {
+            "feature": "Frames",
+            "value": "LightCompose"
+        }
+    ]
+},
+{
+  "id": 40346,
+  "campus": "hr-rfp",
+  "name": "Morning Joggers",
+  "slogan": "Make yourself a morning person",
+  "description": "Whether you're a morning person or not.  Whether you're gym bound or not.  Everyone looks good in joggers.",
+  "category": "Pants",
+  "default_price": "40.00",
+  "created_at": "2021-08-13T14:38:44.509Z",
+  "updated_at": "2021-08-13T14:38:44.509Z",
+  "features": [
+      {
+          "feature": "Fabric",
+          "value": "100% Cotton"
+      },
+      {
+          "feature": "Cut",
+          "value": "Skinny"
+      }
+  ]
+},
+{
+  "id": 40351,
+  "campus": "hr-rfp",
+  "name": "YEasy 350",
+  "slogan": "Just jumped over jumpman",
+  "description": "These stretchy knit shoes show off asymmetrical lacing and a big sculpted rubber midsole. In a nod to adidas soccer heritage.",
+  "category": "Kicks",
+  "default_price": "450.00",
+  "created_at": "2021-08-13T14:38:44.509Z",
+  "updated_at": "2021-08-13T14:38:44.509Z",
+  "features": [
+      {
+          "feature": "Sole",
+          "value": "Rubber"
+      },
+      {
+          "feature": "Material",
+          "value": "FullControlSkin"
+      },
+      {
+          "feature": "Stitching",
+          "value": "Double Stitch"
+      }
+  ]
+},
+{
+  "id": 40350,
+  "campus": "hr-rfp",
+  "name": "Blues Suede Shoes",
+  "slogan": "2019 Stanley Cup Limited Edition",
+  "description": "Touch down in the land of the Delta Blues in the middle of the pouring rain",
+  "category": "Dress Shoes",
+  "default_price": "120.00",
+  "created_at": "2021-08-13T14:38:44.509Z",
+  "updated_at": "2021-08-13T14:38:44.509Z",
+  "features": [
+      {
+          "feature": "Sole",
+          "value": "Rubber"
+      },
+      {
+          "feature": "Material",
+          "value": "FullControlSkin"
+      },
+      {
+          "feature": "Stitching",
+          "value": "Double Stitch"
+      }
+  ]
+}
+]
+
 const initialState = {
   detailView: false,
-  relatedItems: testData,
+  relatedItems: testDataRelatedItems,
   currentCardIndex: 0,
   outfit: []
 
@@ -89,7 +207,9 @@ export const relatedItemsSlice = createSlice({
       }
     },
     addToOutfit: (state, action) => {
-      state.outfit = [...state.outfit, action.payload]
+      if (!state.outfit.includes(action.payload)) {
+        state.outfit = [...state.outfit, action.payload]
+      }
     }
   }
 })
