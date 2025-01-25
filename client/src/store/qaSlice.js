46fea3e4f51fdde814cf2bacbaaa9a6f1f985647
Andrew Myronchuk <myronchukmail@gmail.com>
2025-01-25 15:07:42 -0500

    add more example data

--- client/src/store/qaSlice.js
+++ client/src/store/qaSlice.js
@@ -43,7 +43,48 @@ const exampleData = {
                   ]
               }
           }
-      }
+      },
+      {
+        "question_id": 647192,
+        "question_body": "1How long will the slacker's slacks last?",
+        "question_date": "2023-11-28T00:00:00.000Z",
+        "asker_name": "1Mallo123",
+        "question_helpfulness": 66,
+        "reported": false,
+        "answers": {
+            "5993667": {
+                "id": 5993667,
+                "body": "not long",
+                "date": "2024-01-20T00:00:00.000Z",
+                "answerer_name": "sasha32",
+                "helpfulness": 3,
+                "photos": [
+                    "https://res.cloudinary.com/daakpfwlp/image/upload/v1705736211/ttr5ebvadvo1wgcgurz0.jpg"
+                ]
+            },
+            "5993669": {
+                "id": 5993669,
+                "body": "They last an unusually long time",
+                "date": "2024-01-20T00:00:00.000Z",
+                "answerer_name": "sasha67",
+                "helpfulness": 1,
+                "photos": [
+                    "https://res.cloudinary.com/daakpfwlp/image/upload/v1705785807/wruucnwgvrhq2njfwz1y.jpg"
+                ]
+            },
+            "5994028": {
+                "id": 5994028,
+                "body": "test123321",
+                "date": "2024-10-24T00:00:00.000Z",
+                "answerer_name": "test123321",
+                "helpfulness": 0,
+                "photos": [
+                    "https://res.cloudinary.com/daxozvday/image/upload/v1729788012/qobi2iczf5dcjuoh1jtc.jpg",
+                    "https://res.cloudinary.com/daxozvday/image/upload/v1729788016/fko8syb7ireag9mia0rd.jpg"
+                ]
+            }
+        }
+    }
   ]
 }
 
