69d99bc0315a7a2b4eab29e42a6475e388f7a0bc
Andrew Myronchuk <myronchukmail@gmail.com>
2025-01-24 19:18:58 -0500

    set up QAAnswersList and QAAnswer components

--- client/src/components/QA/QAListItem.jsx
+++ client/src/components/QA/QAListItem.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const QAListItem = ({ question }) => {
+import QAAnswersList from './QAAnswersList.jsx';
 
+const QAListItem = ({ question }) => {
+  return (
+    <div className="qa-list-item">
+
+      <div className="qa-question-container">
+        <h4 className="qa-question">Q: {question.question_body}</h4>
+        <div className="qa-question-info-container">
+          <div>Helpful? <button className="link-button">Yes</button> ({question.question_helpfulness})</div>
+          <div><button className="link-button">Add Answer</button></div>
+        </div>
+      </div>
+
+      <QAAnswersList question={question} />
+
+
+    </div>
+  )
 };
 
 export default QAListItem;
