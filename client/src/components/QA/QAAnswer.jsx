059af4e0d04b74bb721a5ed326878568f9f600fa
Andrew Myronchuk <myronchukmail@gmail.com>
2025-01-26 17:46:25 -0500

    set up QAAnswerPhotos component

--- client/src/components/QA/QAAnswer.jsx
+++ client/src/components/QA/QAAnswer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import QAAnswerPhotos from './QAAnswerPhotos.jsx';
+
 const QAAnswer = ({ answer }) => {
   return (
     <div className="qa-answer-container">
       <p className="qa-answer"><span className="qa-a">A: </span>{answer.body}</p>
+      {answer.photos.length > 0 && <QAAnswerPhotos photos={answer.photos} />}
       <div className="qa-answer-info-container">
         <div>by {answer.answerer_name}, {answer.date}</div>
         <div>Helpful? <button className="link-button">Yes</button> ({answer.helpfulness})</div>
