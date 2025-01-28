4af3e00dc46a40dd52b8eee6457a576ac0a9b759
Andrew Myronchuk <myronchukmail@gmail.com>
2025-01-28 17:41:46 -0500

    set up QAPhotoUploader component

---
+++ client/src/components/QA/QAPhotoUploader.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useState } from 'react';
+
+import axios from 'axios';
+import QANotification from './QANotification.jsx';
+
+const QAPhotoUploader = ({ formData, setFormData }) => {
+  const [isUploading, setIsUploading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleFileInputChange = (e) => {
+    setIsError(false);
+    setIsUploading(true);
+    const photo = (e.target.files[0]);
+    const formData = new FormData();
+    formData.append('file', photo);
+    formData.append('upload_preset', 'hr_fec');
+
+    axios.post('https://api.cloudinary.com/v1_1/dmbs2shkd/image/upload', formData)
+    .then((result) => {
+      setFormData((prev) => {
+        return {
+          ...prev,
+          photos: [...prev.photos, result.data.secure_url],
+          thumbnails: [...prev.thumbnails, `https://res.cloudinary.com/dmbs2shkd/image/upload/c_fill,w_100,h_100/${result.data.public_id}.jpg`]
+        }
+      })
+      setIsUploading(false);
+    })
+    .catch((err) => {
+      console.log(err);
+      setIsUploading(false);
+      setIsError(true);
+    })
+  }
+
+  return (
+    <div className="qa-photo-uploader-container">
+      {
+        isUploading && (
+          <QANotification type="loading" msg="Uploading your photo. Please wait..." />
+        )
+      }
+
+      { (!isUploading && formData.photos.length < 5) ? (
+          <label>Upload a photo <span>(You can attach up to 5 photos)</span>
+            <input type="file" accept="image/*" onChange={handleFileInputChange} />
+          </label>
+        ) : (
+          <QANotification type="notification" msg="You have attached maximum number of photos." />
+        )
+      }
+
+      {
+        isError && (
+          <QANotification type="error" msg="Image upload failed. Please try again." />
+        )
+      }
+
+      {
+        formData.photos.length > 0 && (
+        <div className="qa-photo-uploader-thumbnails-container">
+          { formData.thumbnails.map((thumbnailUrl) => <img src={thumbnailUrl} />) }
+        </div>
+        )
+      }
+    </div>
+  )
+};
+
+export default QAPhotoUploader;
