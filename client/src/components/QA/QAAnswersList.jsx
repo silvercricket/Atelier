968c0fb49154322cb6fd967838aa775f761a10ad
Andrew Myronchuk <myronchukmail@gmail.com>
2025-01-28 19:25:12 -0500

    install date-fns package. modify css and implement report answer feature

--- client/src/components/QA/QAAnswersList.jsx
+++ client/src/components/QA/QAAnswersList.jsx
@@ -4,8 +4,11 @@ import QAAnswer from './QAAnswer.jsx';
 
 const QAAnswersList = ({ question }) => {
   return (
-    <div className="answers-list-container">
-      {Object.keys(question.answers).map((answerId) => <QAAnswer answer={question.answers[answerId]} key={answerId} />)}
+    <div className="qa-answers-list-container">
+      <div className="qa-a">A:</div>
+      <div className="qa-answers-list">
+        {Object.keys(question.answers).map((answerId) => <QAAnswer answer={question.answers[answerId]} key={answerId} />)}
+      </div>
     </div>
   )
 };
